c41fa3f9ee3683b1bc6db32a10ea1340d622a4c1
AvianaLilian <43570065+AvianaLilian@users.noreply.github.com>
2019-04-03 11:51:24 +0200

    Database
    Co-Authored-By: blind0007 <blind0007@users.noreply.github.com>

--- Website/Website/App_Data/CreateDatabase.sql
+++ Website/Website/App_Data/CreateDatabase.sql
@@ -1,10 +1,45 @@
-﻿DROP TABLE Registratie;
+﻿DROP TABLE Images;
+DROP TABLE Comments;
+DROP TABLE Registratie;
+DROP TABLE Stories;
+
 CREATE TABLE Registratie (
- Registratie_Id INT PRIMARY KEY IDENTITY,
- Naam NVARCHAR(50) NOT NULL,
- Gebruikersnaam NVARCHAR(50) NOT NULL,
- Mailadres NVARCHAR(50) NOT NULL,
- Wachtwoord NVARCHAR(50) NOT NULL
+    Id INT IDENTITY (1, 1) NOT NULL,
+    Naam NVARCHAR (50) NOT NULL,
+    Gebruikersnaam NVARCHAR (50) NOT NULL,
+    Mailadres NVARCHAR (50) NOT NULL,
+    Wachtwoord NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED (Id ASC)
 );
 
-INSERT INTO Todo (Item) VALUES ('item2');
+
+
+CREATE TABLE Stories (
+    Id     INT            IDENTITY (1, 1) NOT NULL,
+    Titel  NVARCHAR (100) NOT NULL,
+    Auteur NVARCHAR (50)  NULL,
+    Datum  DATE           NULL,
+    PRIMARY KEY CLUSTERED (Id ASC)
+);
+
+
+CREATE TABLE Comments (
+    Id             INT           IDENTITY (1, 1) NOT NULL,
+    Datum          DATE          NOT NULL,
+    Naam           NVARCHAR (50) NOT NULL,
+    Registratie_Id INT           NOT NULL,
+    Story_Id       INT           NOT NULL,
+    PRIMARY KEY CLUSTERED (Id ASC),
+    FOREIGN KEY (Story_Id) REFERENCES Stories (Id),
+    FOREIGN KEY (Registratie_Id) REFERENCES Registratie (Id)
+);
+
+
+
+CREATE TABLE Images (
+    Id       INT            IDENTITY (1, 1) NOT NULL,
+    ImageURL NVARCHAR (200) NOT NULL,
+    Story_Id INT            NOT NULL,
+    PRIMARY KEY CLUSTERED (Id ASC),
+    FOREIGN KEY (Story_Id) REFERENCES Stories (Id)
+);
