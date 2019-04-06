6b14e93cc658854b9bc2d44050f916f7943afc5d
Shadow_Flame85 <wybren7@gmail.com>
2019-04-06 16:11:53 +0200

    search is functional

---
+++ Website/Website/App_Code/StoryList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for StoryList
+/// </summary>
+public class StoryList
+{
+    public StoryList()
+    {
+        //
+        // TODO: Add constructor logic here
+        string Title;
+        string ImageURL;
+        //
+    }
+}
